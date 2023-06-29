862a23f9fc58af4a486e89d2b8069844ab1a8dc2
Laylalll <yusioulai@gmail.com>
2023-06-29 16:43:25 +0800

    show restaurant detail in show page

--- app.js
+++ app.js
@@ -14,7 +14,10 @@ app.get('/', (req, res) => {
   res.render('index', { restaurants: restaurantList.results })
 })
 
-
+app.get('/restaurants/:restaurant_id', (req, res) => {
+  const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
+  res.render('show', { restaurant: restaurant })
+})
 
 app.listen(port, () => {
   console.log(`Express is running on http://localhost:${port}.`)
