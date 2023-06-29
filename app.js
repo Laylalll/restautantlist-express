302a9543018b06f325817e2eb4eabe9609d8a526
Laylalll <yusioulai@gmail.com>
2023-06-29 17:26:42 +0800

    add search restaurants

--- app.js
+++ app.js
@@ -13,10 +13,17 @@ app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.render('index', { restaurants: restaurantList.results })
 })
-
 app.get('/restaurants/:restaurant_id', (req, res) => {
   const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
   res.render('show', { restaurant: restaurant })
+})
+app.get('/search', (req, res) => {
+  const keyword = req.query.keyword
+  const restaurants = restaurantList.results.filter(restaurant =>
+    restaurant.name.toLowerCase().includes(keyword.toLowerCase()) ||
+    restaurant.category.toLowerCase().includes(keyword.toLowerCase())
+  )
+  res.render('index', { restaurants: restaurants, keyword: keyword })
 })
 
 app.listen(port, () => {
